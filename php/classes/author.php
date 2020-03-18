191807cd148ddfc4b6ab81c55dc4a9a3dd6b7a52
Corrigan Newsome <cnewsome2@cnm.edu>
2020-03-18 11:20:07 -0600

    added class to php file

--- php/classes/author.php
+++ php/classes/author.php
@@ -1,5 +1,5 @@
 <?php
-namespace Deepdivedylan\DataDesign;
+namespace cnewsome2/objectoriented;
 
 require_once("autoload.php");
 require_once(dirname(__DIR__) . "/vendor/autoload.php");
@@ -7,3 +7,17 @@ require_once(dirname(__DIR__) . "/vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
 
 class author
+use ValidateDate;
+use ValidateUuid;
+
+
+
+	public function jsonSerialize() : array {
+	$fields = get_object_vars($this);
+
+	$fields["tweetId"] = $this->tweetId->toString();
+	$fields["tweetProfileId"] = $this->tweetProfileId->toString();
+
+	//format the date so that the front end can consume it
+	$fields["tweetDate"] = round(floatval($this->tweetDate->format("U.u")) * 1000);
+	return($fields);
